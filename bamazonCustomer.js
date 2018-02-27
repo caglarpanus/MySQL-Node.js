33aa955bc39110388f6cc966850e9637669e3d0e
caglarpanus <panuscaglar@gmail.com>
2018-02-27 12:02:45 -0500

    A small fix

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,7 @@ var inquirer = require("inquirer");
 
 var conn = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.envDB_PORT,
+    port: process.env.DB_PORT,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE
