6f4b576f4e05054737c04fb04033021124b5705d
caglarpanus <panuscaglar@gmail.com>
2018-03-02 15:11:15 -0500

    show inventory was created

--- bamazonManager.js
+++ bamazonManager.js
@@ -15,7 +15,6 @@ var conn = mysql.createConnection({
 conn.connect(function(err){
     if(err) throw err;
     console.log("Connected as ID " + conn.threadId);
-   
 });
 
 var manager = function(){
@@ -24,7 +23,8 @@ var manager = function(){
         {
             name:"action",
             type:"list",
-            choices:["View Products for Sale","View Low Inventory","Add to Inventory","Add New Product"]
+            choices:["View Products for Sale","View Low Inventory","Add to Inventory","Add New Product"],
+            message:"Hello, what would you like to do?"
         }
     ]).then(function(answer){
         switch(answer.action){
@@ -50,9 +50,21 @@ var manager = function(){
         }
     });
 };
-
+//The manager wants to see the current inventory.
 function showInventory(){
+    conn.query("SELECT * FROM products", function(err,res){
+        //CLI tabel will help you to see table more clearly.
+        var table = new Table ({
+            head: ["item_id","product_name","department_name","price","stock_quantity"]
+        });
+        console.log("=== HERE IS THE CURRENT INVENTORY ===");
 
+        for(var i = 0; i < res.length; i++){
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity])
+        }
+        console.log(table.toString());
+        showInventory();
+    })
 };
 
 function showLowInventory(){
@@ -66,3 +78,5 @@ function addInventory(){
 function addNewProduct(){
 
 };
+
+manager();
